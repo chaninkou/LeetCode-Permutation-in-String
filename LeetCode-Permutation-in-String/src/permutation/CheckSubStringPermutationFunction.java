11643ce573294a95cd4175af092eeac4ef898535
chaninkou <michaelkoulove@yahoo.com>
2019-11-10 18:27:04 -0800

    Great review. Used the int[26] method to count to check substring.

---
+++ LeetCode-Permutation-in-String/src/permutation/CheckSubStringPermutationFunction.java
@@ -0,0 +1,54 @@
+package permutation;
+
+public class CheckSubStringPermutationFunction {
+    public static boolean checkInclusion(String s1, String s2) {
+    	
+    	// Make sure s1 length is not greater than s2, since we want to check if s2 contain all of s1
+    	if(s1.length() > s2.length()){
+    		return false;
+    	}
+    	
+    	// Since it only contains lower case letters and order does matter so we don't use hashset
+    	int[] char_counts = new int[26];
+    	
+    	// Count up to the s1 length
+    	for(int i = 0; i < s1.length(); i++){
+    		char_counts[s1.charAt(i) - 'a']++;
+    		char_counts[s2.charAt(i) - 'a']--;
+    	}
+    	
+    	// Checking right away if s2 already contain all of s1
+    	if(allZeros(char_counts)){
+    		return true;
+    	}
+    	
+    	// As the i continue, subtract the count as you go
+    	// As the i - s1.length() continue, add the count as you go
+    	for(int i = s1.length(); i < s2.length(); i++){
+    		// Mark as visited
+    		char_counts[s2.charAt(i) - 'a']--;
+    		
+    		// This is revisiting
+    		char_counts[s2.charAt(i - s1.length()) - 'a']++;
+    		
+    		// Since we want substring
+        	// Checking currently if s2 already contain all of s1
+        	if(allZeros(char_counts)){
+        		return true;
+        	}
+    	}
+    	
+    	// Return false when s2 does not contain all of s1 since the array need to be all zero
+    	return false;
+    }
+    
+    private static boolean allZeros(int[] char_counts){
+    	for(int i = 0; i < 26; i++){
+    		if(char_counts[i] != 0){
+    			return false;
+    		}
+    	}
+    	
+    	return true;
+    }
+}
--- LeetCode-Permutation-in-String/src/permutation/Main.java
+++ LeetCode-Permutation-in-String/src/permutation/Main.java
@@ -10,49 +10,9 @@ public class Main {
 		
 		System.out.println("input1: " + input1 + " input2: " + input2);
 		
-		System.out.println("Output: " + checkInclusion(input1, input2));
+		CheckSubStringPermutationFunction solution = new CheckSubStringPermutationFunction();
+		
+		System.out.println("Output: " + solution.checkInclusion(input1, input2));
 			
 	}
-	
-    public static boolean checkInclusion(String s1, String s2) {
-        int s1Len = s1.length();
-        int s2Len = s2.length();
-        
-        // s1 length can not be bigger than s2 length
-        if(s1Len > s2Len){
-            return false;
-        }
-        
-        // Since there are 26 lower case letters
-        int[] count = new int[26];
-        
-        for(int i = 0; i < s1Len; i++){
-            count[s1.charAt(i) - 'a']++;
-            count[s2.charAt(i) - 'a']--;
-        }
-        
-        if(allZero(count)){
-            return true;
-        }
-        
-        for(int i = s1Len; i < s2Len; i++){
-            count[s2.charAt(i) - 'a']--;
-            count[s2.charAt(i - s1Len) - 'a']++;
-            if(allZero(count)){
-                return true;
-            }
-        }
-        
-        
-        return false;
-    }
-    
-    private static boolean allZero(int[] count){
-        for(int i = 0; i < 26; i++){
-            if(count[i] != 0){
-                return false;
-            }
-        }
-        return true;
-    }
 }
